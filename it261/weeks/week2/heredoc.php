b29ea7df23b860e0c2ca53a8f4c3d720a3d1aca7
zhanhost <99058834+zhanhost@users.noreply.github.com>
2023-01-16 17:13:07 -0800

    Delete it261 directory

--- it261/weeks/week2/heredoc.php
+++
@@ -1,6 +0,0 @@
-<?php
-$myDoc = <<<EOD
-One of my favorite books is the Handmaid's Tale, written by Margaret Atwood, and is presently a miniseries on HULU. Hulu's viewing audience is extremely excited about the miniseries and looks forward to the 5th season of the award-winning "Handmaid's Tale!"
-EOD;
-echo $myDoc;
-?>
