7e7fcba6c5c197f62eccf9e59eb5f3a0f674c520
zhanhost <99058834+zhanhost@users.noreply.github.com>
2023-02-27 22:00:08 -0800

    uploaded changes to index.php

--- index.php
+++ index.php
@@ -15,10 +15,10 @@
                 <ul>
                     <li><a href="./website/daily.php">Switch</a></li>
                     <li><a href="troubleshoot.php">Troubleshoot</a></li>
-                    <li><a href="">Calendar</a></li>
+                    <li><a href="calculator.php">Calculator</a></li>
                     <li><a href="">Email</a></li>
                     <li><a href="">Database</a></li>
-                    <li><a href="">Gallery</a></li>
+                    <li><a href="./website/gallery.php">Gallery</a></li>
                 </ul>
             </nav>
         </header>
